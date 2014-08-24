963d3f9e9ddffb26f40db0e8d506c11d973d9ab8
kg <kennytgao@gmail.com>
2014-08-24 03:47:27 -0700

    initial commit

---
+++ desktop/src/me/phra/nascence/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package me.phra.nascence.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import me.phra.nascence.Nascence;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new Nascence(), config);
+	}
+}
